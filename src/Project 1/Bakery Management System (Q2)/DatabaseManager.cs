a844fd9a55161f66978d831974c16374d85dba19
Dihan Britz <dbritz22@proton.me>
2025-04-19 20:04:53 +0200

    Simplified database manager logic

--- src/Project 1/Bakery Management System (Q2)/DatabaseManager.cs
+++ src/Project 1/Bakery Management System (Q2)/DatabaseManager.cs
@@ -2,25 +2,16 @@ using Microsoft.Data.SqlClient;
 
 namespace Bakery_Management_System
 {
-    public class DatabaseManager
+    public class DatabaseManager(string connectionString)
     {
-        private readonly string connectionString;
-
-        public DatabaseManager(string connectionString)
-        {
-            this.connectionString = connectionString;
-        }
-
         // Test the database connection
         public bool TestConnection()
         {
             try
             {
-                using (SqlConnection connection = new SqlConnection(this.connectionString))
-                {
-                    connection.Open();
-                    return true;
-                }
+                using var connection = new SqlConnection(connectionString);
+                connection.Open();
+                return true;
             }
             catch (Exception)
             {
@@ -34,30 +25,24 @@ namespace Bakery_Management_System
 
             try
             {
-                using (SqlConnection connection = new SqlConnection(this.connectionString))
-                {
-                    connection.Open();
-                    string query = "SELECT * FROM Products";
+                using var connection = new SqlConnection(connectionString);
+                connection.Open();
+                var query = "SELECT * FROM Products";
 
-                    // Execute the query and read the results
-                    using (SqlCommand command = new SqlCommand(query, connection))
+                // Execute the query and read the results
+                using var command = new SqlCommand(query, connection);
+                using var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var product = new Product
                     {
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                Product product = new Product
-                                {
-                                    ProductID = Convert.ToInt32(reader["ProductID"]),
-                                    ProductName = reader["ProductName"]?.ToString() ?? string.Empty,
-                                    Category = reader["Category"]?.ToString() ?? string.Empty,
-                                    Price = Convert.ToSingle(reader["Price"]),
-                                    QuantityInStock = Convert.ToInt32(reader["QuantityInStock"]),
-                                };
-                                products.Add(product);
-                            }
-                        }
-                    }
+                        ProductID = Convert.ToInt32(reader["ProductID"]),
+                        ProductName = reader["ProductName"]?.ToString() ?? string.Empty,
+                        Category = reader["Category"]?.ToString() ?? string.Empty,
+                        Price = Convert.ToSingle(reader["Price"]),
+                        QuantityInStock = Convert.ToInt32(reader["QuantityInStock"]),
+                    };
+                    products.Add(product);
                 }
             }
             catch (Exception ex)
@@ -73,29 +58,22 @@ namespace Bakery_Management_System
         {
             try
             {
-                using (SqlConnection connection = new SqlConnection(this.connectionString))
-                {
-                    connection.Open();
-                    string query =
-                        @"INSERT INTO Products (ProductName, Category, Price, QuantityInStock) 
+                using var connection = new SqlConnection(connectionString);
+                connection.Open();
+                string query =
+                    @"INSERT INTO Products (ProductName, Category, Price, QuantityInStock)
                                    VALUES (@ProductName, @Category, @Price, @QuantityInStock);
                                    SELECT SCOPE_IDENTITY();";
 
-                    using (SqlCommand command = new SqlCommand(query, connection))
-                    {
-                        command.Parameters.AddWithValue("@ProductName", product.ProductName);
-                        command.Parameters.AddWithValue("@Category", product.Category);
-                        command.Parameters.AddWithValue("@Price", product.Price);
-                        command.Parameters.AddWithValue(
-                            "@QuantityInStock",
-                            product.QuantityInStock
-                        );
+                using var command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@ProductName", product.ProductName);
+                command.Parameters.AddWithValue("@Category", product.Category);
+                command.Parameters.AddWithValue("@Price", product.Price);
+                command.Parameters.AddWithValue("@QuantityInStock", product.QuantityInStock);
 
-                        // Get the newly created ID and assign it to the product
-                        product.ProductID = Convert.ToInt32(command.ExecuteScalar());
-                        return true;
-                    }
-                }
+                // Get the newly created ID and assign it to the product
+                product.ProductID = Convert.ToInt32(command.ExecuteScalar());
+                return true;
             }
             catch (Exception ex)
             {
@@ -109,32 +87,25 @@ namespace Bakery_Management_System
         {
             try
             {
-                using (SqlConnection connection = new SqlConnection(this.connectionString))
-                {
-                    connection.Open();
-                    string query =
-                        @"UPDATE Products 
-                                   SET ProductName = @ProductName, 
-                                       Category = @Category, 
-                                       Price = @Price, 
-                                       QuantityInStock = @QuantityInStock 
+                using var connection = new SqlConnection(connectionString);
+                connection.Open();
+                string query =
+                    @"UPDATE Products
+                                   SET ProductName = @ProductName,
+                                       Category = @Category,
+                                       Price = @Price,
+                                       QuantityInStock = @QuantityInStock
                                    WHERE ProductID = @ProductID";
 
-                    using (SqlCommand command = new SqlCommand(query, connection))
-                    {
-                        command.Parameters.AddWithValue("@ProductID", product.ProductID);
-                        command.Parameters.AddWithValue("@ProductName", product.ProductName);
-                        command.Parameters.AddWithValue("@Category", product.Category);
-                        command.Parameters.AddWithValue("@Price", product.Price);
-                        command.Parameters.AddWithValue(
-                            "@QuantityInStock",
-                            product.QuantityInStock
-                        );
+                using var command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@ProductID", product.ProductID);
+                command.Parameters.AddWithValue("@ProductName", product.ProductName);
+                command.Parameters.AddWithValue("@Category", product.Category);
+                command.Parameters.AddWithValue("@Price", product.Price);
+                command.Parameters.AddWithValue("@QuantityInStock", product.QuantityInStock);
 
-                        int rowsAffected = command.ExecuteNonQuery();
-                        return rowsAffected > 0;
-                    }
-                }
+                var rowsAffected = command.ExecuteNonQuery();
+                return rowsAffected > 0;
             }
             catch (Exception ex)
             {
@@ -148,19 +119,15 @@ namespace Bakery_Management_System
         {
             try
             {
-                using (SqlConnection connection = new SqlConnection(this.connectionString))
-                {
-                    connection.Open();
-                    string query = "DELETE FROM Products WHERE ProductID = @ProductID";
+                using var connection = new SqlConnection(connectionString);
+                connection.Open();
+                var query = "DELETE FROM Products WHERE ProductID = @ProductID";
 
-                    using (SqlCommand command = new SqlCommand(query, connection))
-                    {
-                        command.Parameters.AddWithValue("@ProductID", productId);
+                using var command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@ProductID", productId);
 
-                        int rowsAffected = command.ExecuteNonQuery();
-                        return rowsAffected > 0;
-                    }
-                }
+                var rowsAffected = command.ExecuteNonQuery();
+                return rowsAffected > 0;
             }
             catch (Exception ex)
             {
@@ -176,35 +143,29 @@ namespace Bakery_Management_System
 
             try
             {
-                using (SqlConnection connection = new SqlConnection(this.connectionString))
-                {
-                    connection.Open();
-                    string query =
-                        @"SELECT * FROM Products 
-                                   WHERE ProductName LIKE @SearchTerm 
-                                   OR Category LIKE @SearchTerm 
+                using var connection = new SqlConnection(connectionString);
+                connection.Open();
+                string query =
+                    @"SELECT * FROM Products
+                                   WHERE ProductName LIKE @SearchTerm
+                                   OR Category LIKE @SearchTerm
                                    OR CAST(ProductID AS VARCHAR) LIKE @SearchTerm";
 
-                    using (SqlCommand command = new SqlCommand(query, connection))
-                    {
-                        command.Parameters.AddWithValue("@SearchTerm", "%" + searchTerm + "%");
+                using var command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@SearchTerm", "%" + searchTerm + "%");
 
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                Product product = new Product
-                                {
-                                    ProductID = Convert.ToInt32(reader["ProductID"]),
-                                    ProductName = reader["ProductName"]?.ToString() ?? string.Empty,
-                                    Category = reader["Category"]?.ToString() ?? string.Empty,
-                                    Price = Convert.ToSingle(reader["Price"]),
-                                    QuantityInStock = Convert.ToInt32(reader["QuantityInStock"]),
-                                };
-                                results.Add(product);
-                            }
-                        }
-                    }
+                using var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var product = new Product
+                    {
+                        ProductID = Convert.ToInt32(reader["ProductID"]),
+                        ProductName = reader["ProductName"]?.ToString() ?? string.Empty,
+                        Category = reader["Category"]?.ToString() ?? string.Empty,
+                        Price = Convert.ToSingle(reader["Price"]),
+                        QuantityInStock = Convert.ToInt32(reader["QuantityInStock"]),
+                    };
+                    results.Add(product);
                 }
             }
             catch (Exception ex)
